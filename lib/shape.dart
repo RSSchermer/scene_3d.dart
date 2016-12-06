e5641dd1705f03d3a3694df3a5cbf411d519dd1b
R.S. Schermer <roland0507@gmail.com>
2016-12-06 11:53:51 +0100

    Remove unused imports

--- lib/shape.dart
+++ lib/shape.dart
@@ -1,12 +1,9 @@
 /// Provides data structures for representing renderable 3D shapes.
 library shape;
 
-import 'package:bagl/geometry_primitives.dart';
-import 'package:bagl/index_geometry.dart';
-import 'package:bagl/math.dart';
+import 'package:bagl/geometry.dart';
 
 import 'material.dart';
-import 'scene.dart';
 import 'transform.dart';
 
 part 'src/shape/shape.dart';
