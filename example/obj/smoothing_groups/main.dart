7f3601415bdcdf307697a335d01ba6d1cb1a6765
R.S. Schermer <roland0507@gmail.com>
2016-12-13 10:25:27 +0100

    Use point light to better illustrate smoothness

--- example/obj/smoothing_groups/main.dart
+++ example/obj/smoothing_groups/main.dart
@@ -16,9 +16,9 @@ main() {
     ..specularColor = new Vector3(0.3, 0.3, 0.3);
 
   loadObj('smooth_cylinder.obj', defaultTrianglesMaterial: material).then((shapes) {
-    var light = new DirectionalLight()
-      ..color = new Vector3(0.9, 0.9, 0.9)
-      ..transform.rotation = new Quaternion.fromEulerAnglesXYZ(0.0, PI, 0.0);
+    var light = new PointLight()
+      ..transform.translation = new Vector3(0.0, 0.0, 10.0)
+      ..quadraticAttenuation = 0.001;
     var camera = new PerspectiveCamera(0.3 * PI, 1.0, 1.0, 100.0)
       ..transform.translation = new Vector3(0.0, 0.0, 20.0);
     var scene = new Scene();
